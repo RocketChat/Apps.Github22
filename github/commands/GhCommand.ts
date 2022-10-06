3ea513bfc772a78902fdf0f47aed56be88b9906c
Henit Chobisa <chobisa.henit@gmail.com>
2022-10-06 18:03:10 +0530

    feat: Gh Slash Command Class Added

---
+++ github/commands/GhCommand.ts
@@ -0,0 +1,35 @@
+import {
+    IRead,
+    IModify,
+    IHttp,
+    IPersistence,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import {
+    ISlashCommand,
+    ISlashCommandPreview,
+    ISlashCommandPreviewItem,
+    SlashCommandContext,
+} from "@rocket.chat/apps-engine/definition/slashcommands";
+
+export class GHCommand implements ISlashCommand {
+    public command: string = "gh";
+    public i18nParamsExample: string = "";
+    public i18nDescription: string = "fetching github data";
+    public providesPreview: boolean = false;
+
+    public async executor(
+        context: SlashCommandContext,
+        read: IRead,
+        modify: IModify,
+        http: IHttp,
+        persis: IPersistence
+    ): Promise<void> {
+        const command = context.getArguments();
+        const sender = context.getSender();
+        const room = context.getRoom();
+
+        if (!Array.isArray(command)) {
+            return;
+        }
+    }
+}
