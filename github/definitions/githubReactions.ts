608bbd99d845393c8e9c362c50fc83c9d5ebf753
Henit Chobisa <chobisa.henit@gmail.com>
2022-11-09 13:39:29 +0530

    feat: Github Issue Schema Modified for Issue Display Modal.

---
+++ github/definitions/githubReactions.ts
@@ -0,0 +1,13 @@
+// reactions object for issues, comments and repos
+
+export interface IGithubReactions {
+    total_count : number,
+    plus_one    : number,
+    minus_one   : number,
+    laugh       : number,
+    hooray      : number,
+    confused    : number,
+    heart       : number,
+    rocket      : number,
+    eyes        : number
+}
