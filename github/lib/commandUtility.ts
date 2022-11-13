89252ac88d639b0a315e76f6ae9f9bc6df366d90
Henit Chobisa <chobisa.henit@gmail.com>
2022-11-14 01:33:23 +0530

    feat : Gist command registered in Command Utility

--- github/lib/commandUtility.ts
+++ github/lib/commandUtility.ts
@@ -23,6 +23,8 @@ import {
 import { handleSearch } from "../handlers/SearchHandler";
 import { handleNewIssue } from "../handlers/HandleNewIssue";
 import { handleUserProfileRequest } from "../handlers/UserProfileHandler";
+import { handleGist, sendGistWithNumber } from "../handlers/handleGist";
+import { getUserGist } from "../helpers/githubSDK";
 
 export class CommandUtility implements ExecutorProps {
     sender: IUser;
@@ -63,6 +65,18 @@ export class CommandUtility implements ExecutorProps {
             });
         } else {
             switch (this.command[0]) {
+                case SubcommandEnum.GIST : {
+                    await handleGist(
+                        this.read,
+                        this.context,
+                        this.app,
+                        this.persistence,
+                        this.http,
+                        this.room,
+                        this.modify
+                    )
+                    break;
+                }
                 case SubcommandEnum.LOGIN: {
                     await handleLogin(
                         this.app,
@@ -154,7 +168,23 @@ export class CommandUtility implements ExecutorProps {
 
     private async handleDualParamCommands() {
         const query = this.command[1];
-        const repository = this.command[0];
+        const command = this.command[0];
+
+
+        if (command == "gist" && command != undefined){
+            await sendGistWithNumber(
+                +query,
+                this.read,
+                this.context,
+                this.app,
+                this.persistence,
+                this.http,
+                this.room,
+                this.modify
+            )
+            return;
+        }
+
         switch (query) {
             case SubcommandEnum.SUBSCRIBE: {
                 SubscribeAllEvents(
@@ -185,7 +215,7 @@ export class CommandUtility implements ExecutorProps {
             default: {
                 await basicQueryMessage({
                     query,
-                    repository,
+                    repository: command,
                     room: this.room,
                     read: this.read,
                     persistence: this.persistence,
