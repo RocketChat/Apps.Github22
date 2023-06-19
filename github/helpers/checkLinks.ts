eb6e2090b4b1f3b24670179fc5828cd170b234f2
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-19 22:02:04 +0530

    feat(Helper): :sparkles: add helper message for code expand

---
+++ github/helpers/checkLinks.ts
@@ -0,0 +1,19 @@
+import { IMessage } from "@rocket.chat/apps-engine/definition/messages";
+
+export async function hasCodeLink(message: IMessage): Promise<Boolean> {
+    let lineNo: RegExp =
+        /https?:\/\/github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_.-]+\/blob\/[A-Za-z0-9_-]+\/.+/;
+
+    if (lineNo.test(message.text!)) {
+        return true;
+    }
+    return false;
+}
+
+export async function isGithubLink(message: IMessage) {
+    let githubLink: RegExp = /(?:https?:\/\/)?(?:www\.)?github\.com\//;
+    if (githubLink.test(message.text!)) {
+        return true;
+    }
+    return false;
+}
