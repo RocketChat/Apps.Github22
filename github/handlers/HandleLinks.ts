59e4063e622ef2f0cd1adda8ba36a3542f52021e
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-29 09:49:43 +0530

    feat(Pr expand feature): :sparkles: added expand PR

--- github/handlers/HandleLinks.ts
+++ github/handlers/HandleLinks.ts
@@ -2,13 +2,14 @@ import { IUser } from "@rocket.chat/apps-engine/definition/users";
 import { IHttp, IRead } from "@rocket.chat/apps-engine/definition/accessors";
 import {
     IMessage,
+    IMessageAction,
     IMessageAttachment,
+    MessageActionButtonsAlignment,
+    MessageActionType,
 } from "@rocket.chat/apps-engine/definition/messages";
 import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
 import { IMessageExtender } from "@rocket.chat/apps-engine/definition/accessors";
 import { TextObjectType } from "@rocket.chat/apps-engine/definition/uikit";
-import { IModify } from "@rocket.chat/apps-engine/definition/accessors/IModify";
-import { ButtonElement } from "@rocket.chat/ui-kit";
 
 async function checkLines(content, url) {
     const regex: RegExp = /(?:L(\d+)+-L(\d+)|L(\d+))/;
@@ -67,13 +68,32 @@ export async function handlePRLink(
     extend: IMessageExtender
 ) {
 
-    
+    const regex: RegExp = /\bhttps?:\/\/github\.com\/\S+\/pull\/\d+\b/;
+    let text = message.text!;
+    const match: RegExpMatchArray | null = text.match(regex);
+    const result: string | undefined = match?.[0];
+    const url = result;
 
+    const regex2: RegExp = /(?:https?:\/\/github\.com\/)(\S+)\/(\S+)\/pull\/(\d+)/;
+    const match2: RegExpMatchArray|undefined | null = url?.match(regex2);
+    const username = match2?.[1];
+    const repositoryName = match2?.[2];
+    const pullNumber = match2?.[3];
+
+
+    
     let attachment: IMessageAttachment = {
+        actionButtonsAlignment: MessageActionButtonsAlignment.HORIZONTAL,
         actions: [
-            
-        ]
+            {
+                type: MessageActionType.BUTTON,
+                text: "More Actions",
+                msg : `/github  ${username}/${repositoryName} pulls ${pullNumber}`,
+                msg_in_chat_window: true,
+            }
+        ],
     };
     extend.addAttachment(attachment);
-
 }
+
+
