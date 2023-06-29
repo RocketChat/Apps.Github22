f91e6f928e54005dc9d74a96d77aac02aa69573c
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-29 11:11:30 +0530

    refactor(Pr expand feature): :fire: remove lines

--- github/handlers/HandleLinks.ts
+++ github/handlers/HandleLinks.ts
@@ -80,15 +80,13 @@ export async function handlePRLink(
     const repositoryName = match2?.[2];
     const pullNumber = match2?.[3];
 
-
-    
     let attachment: IMessageAttachment = {
         actionButtonsAlignment: MessageActionButtonsAlignment.HORIZONTAL,
         actions: [
             {
                 type: MessageActionType.BUTTON,
                 text: "More Actions",
-                msg : `/github  ${username}/${repositoryName} pulls ${pullNumber}`,
+                msg: `/github ${username}/${repositoryName} pulls ${pullNumber}`,
                 msg_in_chat_window: true,
             }
         ],
