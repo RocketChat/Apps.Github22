ede77f82153dcd691bd736fdda08e55db73db830
Jeffrey Yu <35394596+JeffreytheCoder@users.noreply.github.com>
2024-04-13 16:50:49 -0700

    feat: migrate issues GET APIs to GithubSDK

--- github/helpers/githubSDKclass.ts
+++ github/helpers/githubSDKclass.ts
@@ -2,6 +2,7 @@ import { IHttp, HttpStatusCode, IRead } from "@rocket.chat/apps-engine/definitio
 import { UserInfo } from "os";
 import { UserInformation } from "../definitions/Userinfo";
 import { ModalsEnum } from "../enum/Modals";
+import { IGitHubIssue } from "../definitions/githubIssue";
 
 class GitHubApi {
     private http: IHttp;
@@ -17,10 +18,10 @@ class GitHubApi {
     }
 
     private async getRequest(url: string): Promise<any> {
-        const response = await this.http.get(url, {
+        const response = await this.http.get(this.BaseApiHost + url, {
             headers: {
-                Authorization: `token ${this.accessToken}`,
                 "Content-Type": "application/json",
+                ...(this.accessToken && { Authorization: `token ${this.accessToken}` }),
             },
         });
 
@@ -33,9 +34,7 @@ class GitHubApi {
 
     public async getBasicUserInfo(): Promise<UserInformation> {
         try {
-            const response = await this.getRequest(
-                this.BaseApiHost + 'user'
-            );
+            const response = await this.getRequest('user');
             return {
                 username: response.login,
                 name: response.name,
@@ -46,7 +45,114 @@ class GitHubApi {
                 avatar: response.avatar_url
             }
         } catch (error) {
-           throw error;
+            throw error;
+        }
+    }
+
+    public async getIssueTemplates(repoName: string) {
+        try {
+            const response = await this.getRequest(`repos/${repoName}/contents/.github/ISSUE_TEMPLATE`);
+            return {
+                templates: JSON.parse(response.content || "{}"),
+                repository: repoName,
+                template_not_found: false,
+            }
+        } catch (error) {
+            return {
+                template_not_found: true,
+            }
+        }
+    }
+
+    public async getIssueTemplateCode(templateDownloadUrl: string) {
+        try {
+            const response = await this.getRequest(templateDownloadUrl);
+            return {
+                template: response.content || "",
+            }
+        } catch (error) {
+            return {
+                template: "",
+            }
+        }
+    }
+
+    public async getIssueData(
+        repoInfo: String,
+        issueNumber: String,
+    ): Promise<IGitHubIssue> {
+        try {
+            const response = await this.getRequest(`repos/${repoInfo}/issues/${issueNumber}`);
+
+            const getAssignees = (assignees: any[]): string[] => assignees.map((val): string => {
+                return val.login as string;
+            })
+
+            return {
+                issue_id: response.id as string,
+                issue_compact: response.body as string,
+                html_url: response.html_url as string,
+                repo_url: response.repository_url as string,
+                user_login: response.user.login as string,
+                user_avatar: response.user.avatar_url as string,
+                number: response.number as number,
+                title: response.title as string,
+                body: response.body as string,
+                assignees: getAssignees(response.assignees),
+                state: response.state as string,
+                last_updated_at: response.updated_at as string,
+                comments: response.comments as number,
+                reactions: {
+                    total_count: response.reactions["total_count"],
+                    plus_one: response.reactions["+1"],
+                    minus_one: response.reactions["-1"],
+                    laugh: response.reactions["laugh"],
+                    hooray: response.reactions["hooray"],
+                    confused: response.reactions["confused"],
+                    heart: response.reactions["heart"],
+                    rocket: response.reactions["rocket"],
+                    eyes: response.reactions["eyes"]
+                }
+            }
+        } catch (error) {
+            return {
+                issue_compact: "Error Fetching Issue",
+                issue_id: 0
+            }
+        }
+    }
+
+    public async getIssuesComments(
+        repoName: string,
+        issueNumber: string | number
+    ) {
+        try {
+            const response = await this.getRequest(`repos/${repoName}/issues/${issueNumber}/comments`);
+            return {
+                data: response,
+                serverError: false
+            }
+        } catch (error) {
+            return {
+                serverError: true,
+                ...error
+            }
+        }
+    }
+
+    public async getRepositoryIssues(repoName: string) {
+        try {
+            const response = await this.getRequest(`repos/${repoName}/issues`);
+            return {
+                issues: response,
+                repository: repoName,
+                serverError: false
+            }
+        } catch (error) {
+            return {
+                serverError: true,
+                ...error
+            }
         }
     }
 }
